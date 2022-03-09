93b6a10f1d49fc73891aef35559b69b62367b8f6
mp2419 <59872205+mp2419@users.noreply.github.com>
2022-03-09 19:00:29 +0000

    task 2 done, to be tested

--- cube_functions.m
+++ cube_functions.m
@@ -1,25 +1,6 @@
 % *********Functions for task 2 moving cubes **********
-%to do in obj:
-%up and down level, current position
-%********************************REACH    
-
-function reach_cube(obj,gripper_angle_init)
-    open_gripper(obj);
-    move_and_flip();
-    close_gripper(obj);
-end
-
-%********************************MOVE
-
-function move_cube(obj,gripper_angle_final)
-    close_gripper(obj);
-    move_and_flip()
-    open_gripper(obj);
-end
 
 %********************************FLIP SETUP
-
-
 
 %retun initial and final gripper orientation plus required flip on spot       
 function gripper_initial_angle, gripper_final_angle, flip_on_spot = set_flip(obj, cube_pos, final_pos, flip_angle)
@@ -63,9 +44,17 @@ function gripper_initial_angle, gripper_final_angle, flip_on_spot = set_flip(obj
         else
             % TODO
             % recursion or back 90/270
-            % gripper_initial_angle, gripper_final_angle, flip_on_spot_2 = set_flip(cube_pos, final_pos, 90)
-            
-        
+            gripper_init1, gripper_final1, flip_on_spot1 = set_flip(cube_pos, final_pos, 90);
+            gripper_init2, gripper_final2, flip_on_spot2 = set_flip(cube_pos, final_pos, 270);
+            if flip_on_spot1 == 0 %can do at least 90
+                gripper_initial_angle = gripper_init1;
+                gripper_final_angle = gripper_final1;
+                flip_on_spot = 90;
+            else % can do at least 270
+                gripper_initial_angle = gripper_init2;
+                gripper_final_angle = gripper_final2;
+                flip_on_spot = 270;               
+            end
         end
     %--270deg turn
     elseif flip_angle > 180 %pos direction
@@ -84,24 +73,123 @@ function gripper_initial_angle, gripper_final_angle, flip_on_spot = set_flip(obj
 end
 
 
-%********************************MOVE and FLIP 
+%********************************REACH, MOVE and FLIP 
 
-function reach_cube(obj,cube_pos)
-       %open, up, new position and gripper orientation, down, close
-       cubeController.open_gripper();
-       current_pos = robotController.get_current_position();
-       obj.robotController.move_to_positions([[position(1), position(2), obj.UP_level,current ]]);
-       up_cube_pos = [cube_pos(1), cube_pos(2), current_pos(3), cu];
-       down_cube_pos = [cube_pos(1), cube_pos(2), obj.down_level, gripper_angle_final];
+function reach_move_flip(obj,cube_pos, final_pos, gripper_initial_angle, gripper_final_angle)
+
+       %****REACH: open, up, new position and gripper orientation, down, close
+       obj.open_gripper();
+       current_pos = obj.robotController.get_current_position();
+       %up
+       obj.robotController.move_to_positions([[current_pos(1), current_pos(2), obj.UP_level, current_pos(4)]]);
+       %reach
+       up_grip_pos = [cube_pos(1), cube_pos(2), obj.UP_level, gripper_initial_angle];
+       pos_array = trajectory(current_pos, up_grip_pos);
+       controller.move_to_positions(pos_array);
+       %down
+       obj.robotController.move_to_positions([[cube_pos(1), cube_pos(2), obj.DOWN_level, gripper_initial_angle]]);
+       obj.close_gripper();
+       
+       %****MOVE CUBE: up, new position and orientation, down, open, up
+       %up
+       current_pos = obj.robotController.get_current_position();
+       obj.robotController.move_to_positions([[current_pos(1), current_pos(2), obj.UP_level, current_pos(4)]]);
+       %move
+       up_cube_pos = [final_pos(1), final_pos(2), obj.UP_level, gripper_final_angle];
        pos_array = trajectory(current_pos, up_cube_pos);
        controller.move_to_positions(pos_array);
-       controller.move_to_positions([down_cube_pos]);
-end       
+       %down
+       obj.robotController.move_to_positions([[final_pos(1), final_pos(2), obj.DOWN_level, gripper_initial_angle]]);
+       obj.open_gripper();
+       %up
+       current_pos = obj.robotController.get_current_position();
+       obj.robotController.move_to_positions([[current_pos(1), current_pos(2), obj.UP_level, current_pos(4)]]);
+       
+end  
 
-function move_and_flip(obj, final_pos, flip_angle):
-    %open/close
-    if
-    reach_cube()
-    close/open
-    end
-end
+%*******************FLIP ON THE SPOT
+function flip_on_the_spot(obj,position, flip_angle)
+
+            %**INPUT**
+            %pos is x,y coords
+            %flip_angle is amount of flip requested in outwards direction from top
+            % 0 deg is red face up, no flip
+            % allowed values for flip = 0, 90,180,270
+            dir = turnability(position);
+            %********compute flipping mode
+            %turn = flip_angle/90;
+            if flip_angle < 180 %neg direction
+                if dir(2) % neg allowed
+                    gripper_final_angle = obj.grip_inwards;
+                    turn = 1;
+                else
+                    gripper_final_angle = obj.grip_outwards;
+                    turn = 3;
+                end
+            elseif flip_angle == 180 %180°
+                if dir(2) && dir(1) % neg allowed
+                    gripper_final_angle = obj.grip_inwards;
+                    turn = 1;
+                elseif dir(2)
+                    gripper_final_angle = obj.grip_inwards;
+                    turn = 2;
+                else
+                    gripper_final_angle = obj.grip_outwards;
+                    turn = 2;   
+                end
+            elseif flip_angle > 180 %pos direction
+                if dir(1) % neg allowed
+                    gripper_final_angle = obj.grip_outwards;
+                    turn = 1;
+                else
+                    gripper_final_angle = obj.grip_inwards;
+                    turn = 3;
+                end
+            end
+            %********actual flipping
+            %open and go up, turn gripper, down, close, go up, turn, down, open,
+            %up
+            for i=1:turn
+                open_gripper(obj);
+                obj.move_up(obj.robotController.get_current_position());
+                obj.robotController.move_to_positions([[position(1), position(2), obj.UP_level, gripper_final_angle]]);
+                obj.robotController.move_to_positions([[position(1), position(2), obj.DOWN_level, gripper_final_angle]]);
+                obj.close_gripper();
+                obj.robotController.move_to_positions([[position(1), position(2), obj.UP_level, gripper_final_angle]]);
+                if flip_angle == 180 && turn == 1
+                    obj.robotController.move_to_positions([[position(1), position(2), current_pos(3), obj.grip_outwards]]);
+                    obj.robotController.move_to_positions([[position(1), position(2), obj.DOWN_level, obj.grip_outwards]]);
+                    obj.open_gripper();
+                    obj.robotController.move_to_positions([[position(1), position(2), obj.UP_level, obj.grip_outwards]]);
+                else
+                    obj.robotController.move_to_positions([[position(1), position(2), current_pos(3), obj.grip_vertical]]);
+                    obj.robotController.move_to_positions([[position(1), position(2), obj.DOWN_level, obj.grip_outwards]]);
+                    obj.open_gripper();
+                    obj.robotController.move_to_positions([[position(1), position(2), obj.UP_level, gripper_outwards]]);
+                end
+            end 
+        end 
+
+    
+        
+        
+        
+        
+ % ignore below
+ 
+% %********************************REACH    
+% 
+% function reach_cube(obj,gripper_angle_init)
+%     open_gripper(obj);
+%     move_and_flip();
+%     close_gripper(obj);
+% end
+% 
+% %********************************MOVE
+% 
+% function move_cube(obj,gripper_angle_final)
+%     close_gripper(obj);
+%     move_and_flip()
+%     open_gripper(obj);
+% end
+
--- main_funct_cubes.m
+++ main_funct_cubes.m
@@ -1,7 +1,7 @@
 % main function for cube task2
 % main_cube:
 %       grid_to_cm()
-%       flip() (compute flip value)
+%       set_flip() (compute flip value)
 %       open gripper
 %       reach_cube()
 %               move_up(1cube_height)
@@ -13,33 +13,35 @@
 %               move to cube pos 2 (+flip position)
 %               move_down  
 %       open grippers
-%       (flip again if requested)
-%       (move away)
+%       (if requested)- flip_on_the_spot()
+%                           up, orient, down, close, up orient, down, open
+%       move up
 
 
 function main_cube(obj,cube_pos_grid, final_pos_grid, flip_angle)
-%***INPUTS****% 
-% cube pos, final pos = x,y in grid coords
-% flip angle = angle diff in rad (inwards from top) 
-% append height and angle to cube and final pos
+    %***INPUTS****% 
+    % cube pos, final pos = x,y in grid coords
+    % flip angle = angle diff in rad (inwards from top) 
+    % append height and angle to cube and final pos
 
-%***PROCESS***%
-%  convert to cm 
-cube_pos = grid_to_cm(cube_pos_grid);
-final_pos = grid_to_cm(final_pos_grid);
+    %***PROCESS***%
+    %  TO DO : convert to cm 
+     cube_pos = grid_to_cm(cube_pos_grid);
+     final_pos = grid_to_cm(final_pos_grid);
 
-% compute flip given two positions (and robot range), 
-% append to goal positions
-flip = set_flip(cube_pos, final_pos, flip_angle);
+    % compute flip given two positions (and robot range), 
+    % append to goal positions
+    gripper_initial_angle, gripper_final_angle, flip_on_spot = set_flip(obj, cube_pos, final_pos, flip_angle);
 
-% reach_cube
-reach_cube();
+    % reach cube and move, flip if possible
+    reach_move_flip(obj,cube_pos, final_pos, gripper_initial_angle, gripper_final_angle);
 
-% move cube
-move_cube();
-    
+    % rest of flipping performed on the spot
+    flip_on_the_spot(obj,final_pos, flip_on_spot);
+  
 end
 
+%to do after testing: stacking: same ration but different UP and DOWN_level
 % function main_cube_stack(obj,cube_pos, final_pos, flip_angle)
 %     
 % end
