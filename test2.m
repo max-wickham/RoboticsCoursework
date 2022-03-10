0180826fb1a1bffc17f443c8ead7ec79ed431513
Max Wickham <maxwickhammonro@gmail.com>
2022-03-10 11:15:07 +0000

    changes

--- test2.m
+++ test2.m
@@ -1,1 +1,1 @@
-pos = trajectory_angle([0,0,0,0],[0,0,0,pi])
+pos = trajectory_angle([0,0,0,0],[0,0,0,2*pi-0.5])
--- trajectory_angle.m
+++ trajectory_angle.m
@@ -1,9 +1,12 @@
 function pos_array = trajectory_angle(current_pos, final_pos)
+    if final_pos(4) > pi
+        final_pos(4) = final_pos(4) - 2*pi;
+    end
     N = round((final_pos(4) - current_pos(4)) * 10 / pi);
     positions = zeros(N,4);
     angles = linspace(current_pos(4), final_pos(4), N);
     for i = 1:N
-        positions(i)
+        positions(i);
         positions(i,1:4) = [current_pos(1), current_pos(2), current_pos(3), angles(i)];
     end
     pos_array = positions;
